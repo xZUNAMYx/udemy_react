d5c36cc66f0ecf229957f0ead4e93b7af725ea5b
AndresAC <andres_fel1pe@hotmail.com>
2024-02-08 20:25:45 -0500

    Finalizacion de seccion 12 y 13

---
+++ 05-hook-app/tests/hooks/useCounter.test.js
@@ -0,0 +1,15 @@
+// import { renderHook } from "@testing-library/react";
+// import { useCounter } from "../../src/hooks/useCounter";
+
+describe('Test en el useCounter', () => {
+    // test('Debe de retornar los valores por defecto', () => {
+    //     const {result} = renderHook( ()=> useCounter());
+    //     const {counter, decrement, increment, reset} = result.current;
+
+    //     expect(counter).toBe(10);
+    //     expect(decrement).toEqual(expect.any(Function));
+    //     expect(increment).toEqual(expect.any(Function));
+    //     expect(reset).toEqual(expect.any(Function));
+    // });
+    
+});
