1cd20725ca78b1224faace80b284ff317b09a65b
AndresAC <andres_fel1pe@hotmail.com>
2024-02-22 20:30:18 -0500

    Finalizacion de la seccion 15 con router y rutas privadas y publicas

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-
+import { AuthContext } from '../../auth/context/AuthContext';
 
 export const Navbar = () => {
+    const { user, logout } = useContext( AuthContext );
     const navigate = useNavigate();
 
     const onLogout = ()=> {
+        logout();
         navigate('/login', {
             replace: true,
         });
@@ -50,7 +53,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                     
                     <span className='nav-item nav-link text-primary'>
-                        Andres
+                        { user?.names }
                     </span>
 
                     <button 
