a18b328bfdeeff66fa18cc8aaaeb7675320f6571
AndresAC <andres_fel1pe@hotmail.com>
2024-05-10 09:41:39 -0500

    Configuración basica de express para backend

---
+++ 10-calendar-backend/index.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+
+//Crear el servidor de express
+const app = express();
+
+
+//Rutas
+app.get('/', (req: any, res: any) => {  
+    res.json({
+        ok: true
+    })
+})
+
+
+//Escuchar peticiones
+app.listen( 4000, ()=>{
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+})
